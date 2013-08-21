aae5380f194d339485552eabcfc99cdb28f1d59a
Phil Maurer <philmau@gmail.com>
2013-08-21 18:37:17 +0100

    Adding some additional info to the footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div id="footer">
 	<div class="inner">
 		<p class="copyright">Copyright &copy; <?php the_time('Y') ?> Phil Maurer</p>
-		<p class="bloginfo">Design by Phil Maurer</p>
+		<p class="bloginfo">Built by Phil Maurer · Running on <a href="http://wiki.nginx.org/Main">Nginx</a> · Powered by <a href="https://www.linode.com/">Linode</a></p>
 		<div id="footer-left" class="footer-widget"><?php dynamic_sidebar('foot-left') ?></div>
 		<div id="footer-right" class="footer-widget"><?php dynamic_sidebar('foot-right') ?></div>
 	</div>
