4bdcf4cbba68368ebba39c66f525a640c359c61b
Phil Maurer <philmau@gmail.com>
2013-08-22 08:27:40 +0100

    Small layout fix

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div>
 <div id="footer">
 	<div class="inner">
-		<p class="copyright">Copyright &copy; <?php the_time('Y') ?> Phil Maurer</p>
+		<p class="copyright">Copyright &copy; <?php the_time('Y') ?> <b>Phil Maurer</b></p>
 		<p class="bloginfo">Built by Phil Maurer · Running on <a href="http://wiki.nginx.org/Main">Nginx</a> · Powered by <a href="https://www.linode.com/">Linode</a></p>
 		<div id="footer-left" class="footer-widget"><?php dynamic_sidebar('foot-left') ?></div>
 		<div id="footer-right" class="footer-widget"><?php dynamic_sidebar('foot-right') ?></div>
